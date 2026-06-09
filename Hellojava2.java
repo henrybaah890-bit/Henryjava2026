2b28ef1f87c29f31769ac527ab4d9d308384ae36
henrybaah890-bit <henrybaah890@gmail.com>
2026-06-09 14:16:08 +0000

    Add files via upload

---
+++ Hellojava2.java
@@ -0,0 +1,16 @@
+public class Hellojava2 {
+    public static void main(String [] args) {
+        System.out.println("x                                                  x");
+        System.out.println("x                                                  x");
+        System.out.println("x                                                  x");
+        System.out.println("x                                                  x");
+        System.out.println("x                                                  x");
+        System.out.println("x                                                  x");
+        System.out.println("x x x x x x   x x x x x x x            x x x x x x x");
+        System.out.println("x         x   x           x            x           x");
+        System.out.println("x         x   x           x            x           x");
+        System.out.println("x         x   x           x            x           x");
+        System.out.println("x         x   x           x            x           x");
+        System.out.println("x         x   x           x            x           x");
+    }
+}
